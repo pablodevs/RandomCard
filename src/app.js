d4ebe42e13ae6be29601bc7f60332973f635dda2
peibol888 <pabloalamovargas@gmail.com>
2021-10-05 17:23:12 +0000

    comments added to app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  // First declare all the 3 random elements: set, number and color
   let randSet = randElement(["♦", "♥", "♠", "♣"]);
   let randNum = randElement([
     "2",
@@ -25,17 +25,20 @@ window.onload = function() {
   ]);
   let randColor = Math.random() >= 0.5 ? "black" : "red";
 
+  // Then change the color (to randColor) and the value (to randSet) of each set in the card
   let H2s = document.querySelectorAll("h2");
   H2s.forEach(H2 => {
     H2.innerHTML = randSet;
     H2.style.color = randColor;
   });
 
+  // Finally change the value of the card to randNum
   let cardTitle = document.querySelector(".card-title");
   cardTitle.innerHTML = randNum;
   cardTitle.style.color = "black";
 };
 
+// Function that return a random element of any array
 const randElement = array => {
   return array[Math.floor(Math.random() * array.length)];
 };
