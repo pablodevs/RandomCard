76690ecaa921afcb74998241b89510ba4f9c6e63
peibol888 <pabloalamovargas@gmail.com>
2021-10-05 17:18:20 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -7,7 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randSet = randElement(["♦", "♥", "♠", "♣"]);
+  let randNum = randElement([
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ]);
+  let randColor = Math.random() >= 0.5 ? "black" : "red";
+
+  let H2s = document.querySelectorAll("h2");
+  H2s.forEach(H2 => {
+    H2.innerHTML = randSet;
+    H2.style.color = randColor;
+  });
+
+  let cardTitle = document.querySelector(".card-title");
+  cardTitle.innerHTML = randNum;
+  cardTitle.style.color = "black";
 };
 
-// ♦ ♥ ♠ ♣
+const randElement = array => {
+  return array[Math.floor(Math.random() * array.length)];
+};
